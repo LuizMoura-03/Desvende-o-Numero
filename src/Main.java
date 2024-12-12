cb379a8b59b02656bf05bc408794c85ef1336213
LuizFernando <barbosa.luiz@zup.com.br>
2024-12-12 16:11:30 -0300

    Creating objects na main

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,22 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.println("### bem vindo ao Game 'Desvende o Número' ###");
+        System.out.println("Digite seu nome: ");
+        String nome = scanner.nextLine();
 
+        Jogador jogador = new Jogador(nome);
+        Jogo jogo = new Jogo();
+
+        boolean continurJogando = true;
+
+        while (continurJogando) {
+            //Escolher o nivel de dificuldade.
+            int limiteSuperior = jogo.escolhaONivel(scanner);
+
+        }
     }
 }
